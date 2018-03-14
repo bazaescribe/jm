65cdbea4c363654c7d1a6bb32512e430a13113bd
José Luis <jlrosasb@gmail.com>
2018-03-14 15:46:50 -0600

    Changing styles and algorithm

--- res/js/zones.js
+++ res/js/zones.js
@@ -1,12 +1,15 @@
 function sort(){
   var zonesCDMX = [];
   var zonesGDL = [];
+  var zonesEMX = [];
 
   for (var i = 0; i < zones.length; i++) {
-    if (zones[i].city == "CDMX") {
+    if (zones[i].city == "CDMX" && zones[i].status == "ON") {
       zonesCDMX.push(zones[i].name);
-    } else if (zones[i].city == "GDL") {
+    } else if (zones[i].city == "GDL" && zones[i].status == "ON") {
       zonesGDL.push(zones[i].name);
+    } else if(zones[i].city == "EMX" && zones[i].status == "ON"){
+      zonesEMX.push(zones[i].name);
     }
   }
 
@@ -14,13 +17,14 @@ function sort(){
   zonesGDL = zonesGDL.sort();
 
   build("Ciudad de México", zonesCDMX);
+  build("Estado de México", zonesEMX);
   build("Guadalajara", zonesGDL);
 }
 
 function build(city, array){
   var contentColumn = "<div class='row'> <div class='col-md-12'> <h2 class='font-black' style='width: 100%'>" + city + "</h2> </div> </div>";
 
-  var columnSize = parseInt(array.length / 3) + 1;
+  var columnSize = parseInt(array.length / 3);
   var columnCount = 0;
 
   contentColumn += "<div class='row' style='margin-bottom: 66px;'>";
