a68379518b87a8022ebd3c3c55062f976d5b4e94
José Luis <jlrosasb@gmail.com>
2018-04-03 20:47:01 -0500

    Uploaded real photos and changed the offices site

---
+++ res/js/quotes.js
@@ -0,0 +1,32 @@
+function quotes(){
+	var testimonial = [
+		{
+			"quote": "Muy buen atención y servicio. Estamos muy contentos con la Sr Guadalupe.",
+			"name": "Adrián Campos",
+			"position": "Comunicación y Cultura",
+			"company": "Dentalia",
+			"logo": "dentalia.png"
+		},
+		{
+			"quote": "Aplausos a Jelpmi y a Laura nuestra querida keeper!! Tenerlos en nuestra oficina los hace parte de nuestra familia.",
+			"name": "Susana Flores",
+			"position": "Undefined",
+			"company": "Correduría 81",
+			"logo": "correduria.png"
+		}
+	];
+	
+	var number 		=  Math.floor(Math.random() * 2);
+	var quote 		= testimonial[number].quote;
+	var name 			= testimonial[number].name;
+	var position 	= testimonial[number].position;
+	var company 	= testimonial[number].company;
+	var logo 			= testimonial[number].logo;
+	
+	console.log(number + quote + name + position + company + logo);	
+	console.log(number);
+	
+	document.getElementById('name').innerHTML = name;
+	document.getElementById('quote').innerHTML = quote;
+	document.getElementById('company').innerHTML = company;
+}
