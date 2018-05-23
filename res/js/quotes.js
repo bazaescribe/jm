00f5ecd50a9de4626de443f17fc3e385f22b2d67
José Luis <jlrosasb@gmail.com>
2018-05-23 09:24:30 -0500

    Updated site

--- res/js/quotes.js
+++ res/js/quotes.js
@@ -13,10 +13,34 @@ function quotes(){
 			"position": "Undefined",
 			"company": "Correduría 81",
 			"logo": "correduria.png"
+		},
+		{
+			"quote": "Jelpmi nos permite seguir dando el mejor servicio a nuestros pacientes ya que su servicio es seguro y confiable, sin duda son los expertos en limpieza.",
+			"name": "Mirtha Sanchez",
+			"position": "Undefined",
+			"company": "veritest",
+			"logo": "veritest.png"
+		},
+		{
+			"quote": "Pedir servicio de jelpmi en nuestra oficina es saber que vamos a tener la tranquilidad que todo estará impecable y a tiempo.",
+			"name": "Yunuen Magaña",
+			"position": "Directora de Experiencia",
+			"company": "Victoria147",
+			"logo": "victoria147.png"
+		},
+		{
+			"quote": "Las keepers son puntuales, amables y se adaptan a las necesidades de cada cafetería. Sin duda es una empresa que te brinda seguridad y seriedad, pero principalmente, ellas se notan muy contentas de realizar su trabajo.",
+			"name": "Jorge Martínez",
+			"position": "Undefined",
+			"company": "Moronas Café",
+			"logo": "moronas.png"
 		}
 	];
 	
-	var number 		=  Math.floor(Math.random() * 2);
+	
+
+	var size 			= testimonial.length;
+	var number 		=  Math.floor(Math.random() * size);
 	var quote 		= testimonial[number].quote;
 	var name 			= testimonial[number].name;
 	var position 	= testimonial[number].position;
